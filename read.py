2e0367e7b3b56d5ce77ecf0dfb4010a701d6562f
Juan Sebastián Pérez Aponte <js.perez20@uniandes.edu.co>
2018-10-30 00:45:52 -0500

    Add files via upload

--- read.py
+++ read.py
@@ -4,11 +4,27 @@ import pandas as pd
 import h5py
 import os
 #%%
-[x[2] for x in os.walk('.')]
+#dirs = np.array([x[1] for x in os.walk('.')])
 
+# dirs = next(os.walk('.'))[1]
+# dirs
+# for dir in dirs:
+#     dirs1 = next(os.walk('./'+dir))[1]
+#     for dir1 in dirs1:
+#         archs = [x for x in os.listdir('./'+dir+'./'+dir1) if x.endswith('.h5')]
+#         data = np.zeros((len(archs), 203, 135))
+#         for ar in archs:
+#             with h5py.File('./'+dir+'./'+dir1+'./'+ar, "r") as hdf:
+#                 items = list(hdf.items())
+#                 datos = hdf.get(u'mod04')
+#                 datos_items = datos.items()
+#                 print(datos_items)
+#                 GL_fields = hdf.get("/mod04/Geolocation Fields")
+#                 longs = np.array(GL_fields.get(u'Longitude'))
+#                 lats = np.array(GL_fields.get(u'Latitude'))
+#                 print(GL_fields.items())
 
-#%%
-with h5py.File("MYD04_3K.A2015006.1900.061.2018046042234.h5", "r") as hdf:
+with h5py.File(".\2012\001\MYD04_L2.A2012001.1755.061.2018036234841.h5", "r") as hdf:
     items = list(hdf.items())
     datos = hdf.get(u'mod04')
     datos_items = datos.items()
@@ -17,6 +33,7 @@ with h5py.File("MYD04_3K.A2015006.1900.061.2018046042234.h5", "r") as hdf:
     longs = np.array(GL_fields.get(u'Longitude'))
     lats = np.array(GL_fields.get(u'Latitude'))
     print(GL_fields.items())
+    
 #%%
 longs.shape
 lats.shape
