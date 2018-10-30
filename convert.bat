d968f1856fc22dd0a29138682fb0512d10500eab
Juan Sebastián Pérez Aponte <js.perez20@uniandes.edu.co>
2018-10-29 22:40:47 -0500

    Add files via upload

---
+++ convert.bat
@@ -0,0 +1,2 @@
+for /r %%v in (*.hdf) do C:"\Program Files\HDF_Group\H4H5\2.2.4\bin\h4toh5convert.exe" "%%v"
+"
---
+++ read.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import h5py
+import os
+#%%
+[x[2] for x in os.walk('.')]
+
+
+#%%
+with h5py.File("MYD04_3K.A2015006.1900.061.2018046042234.h5", "r") as hdf:
+    items = list(hdf.items())
+    datos = hdf.get(u'mod04')
+    datos_items = datos.items()
+    print(datos_items)
+    GL_fields = hdf.get("/mod04/Geolocation Fields")
+    longs = np.array(GL_fields.get(u'Longitude'))
+    lats = np.array(GL_fields.get(u'Latitude'))
+    print(GL_fields.items())
+#%%
+longs.shape
+lats.shape
+#%%
+plt.scatter(longs, lats)
+
+
+
+
+plt.show()
